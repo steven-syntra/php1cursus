9e8207a342acfa6e519d0d697caef1d196361640
Steven De Ryck <steven@inform.be>
2020-12-08 13:33:07 +0100

    Commit Dag 5 - Oef3.1

---
+++ oef3.1/steden2.php
@@ -0,0 +1,32 @@
+<?php
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
+require_once "lib/pdo.php";
+require_once "lib/html_functions.php";
+
+PrintHead();
+PrintJumbo( $title = "Leuke plekken in Europa" ,
+                        $subtitle = "Tips voor citytrips voor vrolijke vakantiegangers!" );
+?>
+
+<div class="container">
+    <div class="row">
+
+<?php
+    //get data
+    $data = GetData( "select * from images" );
+
+    //get template
+    $template = file_get_contents("templates/column.html");
+
+    //merge
+    $output = MergeViewWithData( $template, $data );
+    print $output;
+?>
+
+    </div>
+</div>
+
+</body>
+</html>
