f281529db9e288cb7fe79a7ff41f28ad413dd01f
Steven De Ryck <steven@inform.be>
2020-12-10 13:12:38 +0100

    Dag 6 - Oef3.2

---
+++ oef3.2/lib/html_functions.php
@@ -0,0 +1,66 @@
+<?php
+function PrintHead()
+{
+    $head = file_get_contents("templates/head.html");
+    print $head;
+}
+
+function PrintJumbo( $title = "", $subtitle = "" )
+{
+    $jumbo = file_get_contents("templates/jumbo.html");
+
+    $jumbo = str_replace( "@jumbo_title@", $title, $jumbo );
+    $jumbo = str_replace( "@jumbo_subtitle@", $subtitle, $jumbo );
+
+    print $jumbo;
+}
+
+function MergeViewWithData( $template, $data )
+{
+    $returnvalue = "";
+
+    foreach ( $data as $row )
+    {
+        $output = $template;
+
+        foreach( array_keys($row) as $field )  //eerst "img_id", dan "img_title", ...
+        {
+            $output = str_replace( "@$field@", $row["$field"], $output );
+        }
+
+        $returnvalue .= $output;
+    }
+
+    return $returnvalue;
+}
+
+function MergeViewWithExtraElements( $template, $selects )
+{
+    foreach ( $selects as $key => $select )
+    {
+        $template = str_replace( "@$key@", $select, $template );
+    }
+    return $template;
+}
+
+function MergeViewWithErrors( $template, $errors )
+{
+    foreach ( $errors as $key => $error )
+    {
+        $template = str_replace( "@$key@", "<p style='color:red'>$error</p>", $template );
+    }
+    return $template;
+}
+
+function RemoveEmptyErrorTags( $template, $data )
+{
+    foreach ( $data as $row )
+    {
+        foreach( array_keys($row) as $field )  //eerst "img_id", dan "img_title", ...
+        {
+            $template = str_replace( "@$field" . "_error@", "", $template );
+        }
+    }
+
+    return $template;
+}
