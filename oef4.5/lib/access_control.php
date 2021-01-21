61def9c5561492ff356466bc8534223527912b16
Steven De Ryck <steven@inform.be>
2021-01-21 13:04:07 +0100

    Oefening 4.5 Logout - UserMenu

---
+++ oef4.5/lib/access_control.php
@@ -0,0 +1,21 @@
+<?php
+CheckAccess();
+
+function CheckAccess()
+{
+    global $public_access;
+
+    if ( ! $public_access AND ! isset($_SESSION['user']) )
+    {
+        GoToNoAccess();
+    }
+}
+
+function GoToNoAccess()
+{
+    global $app_root;
+
+    header("Location: " . $app_root . "/no_access.php");
+    exit;
+}
+
